0f1772f1853cb737742dcb0162a7822c7be47187
Navin Kumar <navin143ritu@gmail.com>
2026-07-15 09:45:26 +0530

    Fixed Supabase API key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   "https://ygupjsnezqlxfoxxoddg.supabase.co",
-  "YOUR_PUBLISHABLE_ANON_KEY"
+  "sb_publishable_fiUETfehDxr-UITZMUGubA_NKlNMFhU"
 );
